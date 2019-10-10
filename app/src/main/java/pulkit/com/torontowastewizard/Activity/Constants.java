8f80faf4e978d0b2d6a10daa611db148e1ad1b6a
Siddhesh <sidd@192.168.0.6>
2019-10-10 10:52:45 +0530

    Created new file for constants and change string of register activiyt

---
+++ app/src/main/java/pulkit/com/torontowastewizard/Activity/Constants.java
@@ -0,0 +1,7 @@
+package pulkit.com.torontowastewizard.Activity;
+
+public class Constants {
+
+    public static String DOMAIN = "https://pulkitkumar.me/api";
+    public static String REGISTER = "/register.php";
+}
