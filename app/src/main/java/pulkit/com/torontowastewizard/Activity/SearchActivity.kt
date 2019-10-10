eb9454daa6e6f3d1d252b85b4c138f1d1c7acdc6
droidpulkit <pulkitkumar190@gmail.com>
2019-10-10 01:04:54 -0400

    SearchActivity DataBinding added

--- app/src/main/java/pulkit/com/torontowastewizard/Activity/SearchActivity.kt
+++ app/src/main/java/pulkit/com/torontowastewizard/Activity/SearchActivity.kt
@@ -6,39 +6,38 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import java.util.ArrayList
 import pulkit.com.torontowastewizard.Adapter.WasteAdapter
 import pulkit.com.torontowastewizard.Model.Waste
 import pulkit.com.torontowastewizard.R
+import pulkit.com.torontowastewizard.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
 
-    lateinit var wasteRecyclerView: RecyclerView
+    companion object {
+        private val TAG: String = SearchActivity::class.java.simpleName
+    }
 
-    lateinit var wasteArrayList : ArrayList<Waste>
+    private lateinit var binding : ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
 
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        val intent = intent
-        wasteArrayList = intent.getSerializableExtra("wasteArrayList") as ArrayList<Waste>
-
-        wasteRecyclerView = findViewById(R.id.wasteRecyclerView)
-
+        val wasteArrayList = intent.getSerializableExtra("wasteArrayList") as ArrayList<Waste>
+        val adapter = WasteAdapter(wasteArrayList)
         val layoutManager = LinearLayoutManager(this)
-        wasteRecyclerView.layoutManager = layoutManager
-
         val itemDecor = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         itemDecor.setDrawable(this.resources.getDrawable(R.drawable.line_divider))
-        wasteRecyclerView.addItemDecoration(itemDecor)
 
-        val adapter = WasteAdapter(wasteArrayList)
-        wasteRecyclerView.adapter = adapter
-
-        //        ParkingReportAdapter adapter = new ParkingReportAdapter(wasteArrayList);
-        //        wasteRecyclerView.setAdapter(adapter);
+        binding.wasteRecyclerView.apply {
+            this.layoutManager = layoutManager
+            addItemDecoration(itemDecor)
+            this.adapter = adapter
+        }
     }
 }
