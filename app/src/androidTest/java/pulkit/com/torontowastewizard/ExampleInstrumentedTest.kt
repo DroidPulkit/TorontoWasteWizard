7ac0c9c55d7e2b0dbf6416fc7d26db45276f45cc
droidpulkit <pulkitkumar190@gmail.com>
2019-10-10 19:17:06 -0400

    Converted Test cases from Java to Kotlin
    Signed-off-by: droidpulkit <pulkitkumar190@gmail.com>

---
+++ app/src/androidTest/java/pulkit/com/torontowastewizard/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package pulkit.com.torontowastewizard
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        //Context appContext = InstrumentationRegistry.getTargetContext();
+
+        //assertEquals("pulkit.com.torontowastewizard", appContext.getPackageName());
+    }
+}
